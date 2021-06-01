412fc801e0cbf1613bb85cd80a96811c8bef45c2
Gaelle Bouchenot <gaelle.bouchenot@etu.univ-lyon1.fr>
2021-06-01 09:50:56 +0200

    traduction

--- tennis-app/resources/views/layouts/navigation.blade.php
+++ tennis-app/resources/views/layouts/navigation.blade.php
@@ -41,7 +41,7 @@
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                {{ __('Log Out') }}
+                                {{ __('Déconnexion') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
@@ -83,7 +83,7 @@
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
-                        {{ __('Log Out') }}
+                        {{ __('Déconnexion') }}
                     </x-responsive-nav-link>
                 </form>
             </div>
