f3a02e875c0dc9fdeb0c0c3f1be21ca9ab645355
Gaelle Bouchenot <gaelle.bouchenot@etu.univ-lyon1.fr>
2021-06-01 11:49:38 +0200

    Liste joueur

--- tennis-app/resources/views/joueurs.blade.php
+++ tennis-app/resources/views/joueurs.blade.php
@@ -1,3 +1,5 @@
+<?php use App\Models\Joueur; ?>
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -10,7 +12,30 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    Joueur
+                    <h3 class="font-semibold text-xl text-gray-800 leading-tight">
+                        Les Joueurs
+                    </h3>
+                    <div>
+
+                        <?php
+                        $joueurs = DB::table('Joueur')->get();
+                        echo '<ul>';
+                        foreach ($joueurs as $joueur){
+                            echo '<li>';
+                            echo '<p>';
+                            echo $joueur->nom.' '.$joueur->prenom;
+                            echo '</p>';
+                            echo '</li>';
+                        }
+                        echo '</ul>';
+                        ?>
+                        
+                    </div>
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <x-button name="createJoueur" class="createJoueur" type="button">
+                        Créer un joueur
+                    </x-button> 
                 </div>
             </div>
         </div>
