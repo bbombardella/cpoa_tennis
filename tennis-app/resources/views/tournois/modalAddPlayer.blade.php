f752e1c03d3a4ba13cc8f172d7ece53bbd9ec6e1
Dupre <romain.dupre-lhotel@etu.univ-lyon1.frromain.dupre-lhotel@etu.univ-lyon1.fr>
2021-06-08 11:51:05 +0200

    c'est moche

--- tennis-app/resources/views/tournois/modalAddPlayer.blade.php
+++ tennis-app/resources/views/tournois/modalAddPlayer.blade.php
@@ -13,7 +13,7 @@
 
 <!-- Modal Structure -->
 <!-- This example requires Tailwind CSS v2.0+ -->
-<div class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+<div class="fixed z-10 inset-0 overflow-y-auto overflow-x-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
   <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
     <!--
       Background overlay, show/hide based on modal state.
@@ -40,67 +40,54 @@
         From: "opacity-100 translate-y-0 sm:scale-100"
         To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
     -->
-    <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-      <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-        <div class="sm:flex sm:items-start">
-          <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-            <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
-              Ajouter des joueurs
-            </h3>
-            <div class="mt-2">
-              <p class="text-sm text-gray-500">
-                Veuillez selectionner les participants
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <form method="POST" action="{{ url('/tournois/'.$data['tournois']->id.'/joueurs/associate') }}">
-
-      <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row">
-        <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row">
-            <div>
-                <x-label for="Joueur" :value="__('Joueur')" />
-
-                <div class="col-sm-9">
-                    <select multiple="multiple" id="idPlayer" class="block mt-1 mr-3" type="string" name="joueur[]" required autofocus>
-                            @foreach ($data['joueurs'] as $joueur)
-                            @CRSFs
-                                <li>
-                                    <p>
-                                        <option value="{{$joueur->id}}">{{ $joueur->nom }} {{ $joueur->prenom }}</option>
-                                    </p>
-                                </li>
-                            @endforeach
-                    </select>
+    <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-x-auto shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div class="sm:flex sm:items-start">
+                <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+                        Ajouter des joueurs
+                    </h3>
+                    <div class="mt-2">
+                        <p class="text-sm text-gray-500">
+                            Veuillez selectionner les participants
+                        </p>
+                    </div>
                 </div>
-
+            </div>
         </div>
+    <form method="POST" action="{{ url('/tournois/'.$data['tournois']->id.'/joueurs/associate') }}">
 
-        
-      <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-      <input type="submit" name="validation" value="Ajouter" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm">
-        <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-        <a href="{{ url('/tournois') }}">
-          Annuler
-        </a>
-        </button>
-      </div>
+        <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row">
+            <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row">
+                <div>
+                    <x-label for="Joueur" :value="__('Joueur')" />
+                    <div class="col-sm-9">
+                        <select multiple="multiple" id="idPlayer" class="block mt-1 mr-3" type="string" name="joueur[]" required autofocus>
+                                @foreach ($data['joueurs'] as $joueur)
+                                @CRSFs
+                                    <li>
+                                        <p>
+                                            <option value="{{$joueur->id}}">{{ $joueur->nom }} {{ $joueur->prenom }}</option>
+                                        </p>
+                                    </li>
+                                @endforeach
+                        </select>
+                    </div>
+                </div>
+            <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+        <input type="submit" name="validation" value="Ajouter" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm">
+    </div>
+    </form>
 
-
-
-        </div>
-      </form>
-      <form method="POST" action="{{ url('/tournois/'.$data['tournois']->id.'/joueurs/dissociate') }}">
-      <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row">
-      <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row">
-      <input type="submit" name="removePlayer" value="Retirer un joueur" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm">
+    <form method="POST" action="{{ url('/tournois/'.$data['tournois']->id.'/joueurs/dissociate') }}">
+    <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row">
+        <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row">
+        <input type="submit" name="removePlayer" value="Retirer" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm">           
             <div>
                 <x-label for="Joueur_tournoi" :value="__('joueur_tournoi')" />
 
                 <div class="col-sm-9">
                     <select multiple="multiple" id="idPlayer" class="block mt-1 mr-3" type="string" name="joueur[]" required autofocus>
-
                             @forelse ($data['joueur_tournoi'] as $joueur)
                             @CRSFs
                                 <li>
@@ -114,9 +101,13 @@
                     </select>
                 </div>
             </div>
-            <input type="submit" name="retirer joueur" value="Ajouter" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm">
         </div>
       </form>
     </div>
-  </div>
+    </div>
+  <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+  <a underlined=true href="{{ url('/tournois') }}">
+          Annuler
+        </a>
+    </button>
 </div>
