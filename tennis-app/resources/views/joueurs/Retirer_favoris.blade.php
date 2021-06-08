8f7292e3d88a9fa756c36a10190014257bf08527
Gaelle Bouchenot <gaelle.bouchenot@etu.univ-lyon1.fr>
2021-06-08 09:48:37 +0200

    gestion bouton favoris joueur

--- tennis-app/resources/views/joueurs/Retirer_favoris.blade.php
+++ tennis-app/resources/views/joueurs/Retirer_favoris.blade.php
@@ -1,4 +1,4 @@
-<form method="post" action='{{ url("/joueurs/favoris/remove/$joueur->id") }}'>
+<form method="post" action='{{ url("/joueurs/favoris/remove/".$data['joueur']->id) }}'>
     <x-button name="Remove" class="addFav" type="submit">
         Retirer des favoris
     </x-button>               
