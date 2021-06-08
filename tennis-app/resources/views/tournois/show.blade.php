a6c65819ca8e5e244d2ac8cc513921c2ce8b970c
BASTIEN BOMBARDELLA <bastien.bombardella@etu.univ-lyon1.fr>
2021-06-08 11:05:06 +0200

    Gestion tours tournois

--- tennis-app/resources/views/tournois/show.blade.php
+++ tennis-app/resources/views/tournois/show.blade.php
@@ -36,10 +36,11 @@
                             </li>
                         </ul>
                     </div>
-                    <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-                    <a href="{{ url("/tournois/{$tournoi->id}/tour") }}">
-                      Voir les tours
-                    </a>
+                    <a class="waves-effect waves-light btn modal-trigger" href="{{ url('/tournois/'.$tournoi->id.'/tour') }}">
+                        <x-button name="createJoueur" class="createJoueur mt-5" type="button">
+                            Voir les tours
+                        </x-button> 
+                    </a> 
                     <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
                     <a href="{{ url("/tournois/{$tournoi->id}/joueurs") }}">
                         Voir les joueurs
