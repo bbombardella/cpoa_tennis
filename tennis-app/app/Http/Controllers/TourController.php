8b74dfb8e6ebb1ca87b9ecd76a014c1f5184d2d3
BASTIEN BOMBARDELLA <bastien.bombardella@etu.univ-lyon1.fr>
2021-06-09 08:29:59 +0200

    Gestion cas spécifiques tours

--- tennis-app/app/Http/Controllers/TourController.php
+++ tennis-app/app/Http/Controllers/TourController.php
@@ -15,16 +15,24 @@ class TourController extends Controller
         }
         $tours = Tour::where('idTournois', $id_tournois)->get();
         $enough_player = (((count($tournois->joueur)-1) & count($tournois->joueur))==0 && count($tournois->joueur)!=0);
+        $nb_joueurs = count($tournois->joueur);
+        $nb_tours = ($nb_joueurs)/2;
+        $nb_tours_dispo = $nb_tours - count($tours);
         return view('tour/list')->with('data', [
             'tournois' => $tournois,
+            'nb_joueurs' => $nb_joueurs,
             'enough_player' => $enough_player,
-            'tours' => $tours
+            'tours' => $tours,
+            'nb_tours_dispo' => $nb_tours_dispo
         ]);
     }
 
     public function show($id_tournois, $id_tour) {
         $tour = Tour::find($id_tour);
-        return view('tour/show')->with('tour', $tour);
+        return view('tour/show')->with('data', [
+            "id_tournois" => $id_tournois,
+            "tour" => $tour
+        ]);
     }
 
     public function store() {
