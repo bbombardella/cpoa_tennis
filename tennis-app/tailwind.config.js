a36e1ba4c76995bc4711455ef2c663ee4444236e
BASTIEN BOMBARDELLA <bastien.bombardella@etu.univ-lyon1.fr>
2021-06-09 21:54:57 +0200

    Compilation tailwindcss mode sombre

--- tennis-app/tailwind.config.js
+++ tennis-app/tailwind.config.js
@@ -1,6 +1,9 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+
+    darkMode: 'media',
+
     purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
