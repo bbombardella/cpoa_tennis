dd1712749310491b990e15420b1f3d5f481a6401
Gaelle Bouchenot <gaelle.bouchenot@etu.univ-lyon1.fr>
2021-06-15 11:00:29 +0200

    aie aie aie

--- tennis-app/resources/views/tournois/modalAddPlayer.blade.php
+++ tennis-app/resources/views/tournois/modalAddPlayer.blade.php
@@ -1,4 +1,4 @@
-@include('tournois/listPlayer')
+@include('tournois/show')
 
 <!-- Modal Structure -->
 <!-- This example requires Tailwind CSS v2.0+ -->
@@ -93,7 +93,7 @@
             </div>
             <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                 <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-                    <a href="{{ url('/tournois/'.$data['tournois']->id.'/joueurs') }}">
+                    <a href="{{ url('/tournois/'.$data['tournois']->id) }}">
                       Valider
                     </a>
                 </button>
--- tennis-app/resources/views/tournois/show.blade.php
+++ tennis-app/resources/views/tournois/show.blade.php
@@ -75,11 +75,12 @@
                             Changer le statut
                         </x-button> 
                     </a> 
+                    <a class="waves-effect waves-light btn modal-trigger" href="{{ url('/tournois/'.$data['tournois']->id.'/joueurs/associate') }}">
+                        <x-button name="createTournoi" class="createTournoi mt-5" type="button">
+                            Modifier les participants
+                        </x-button> 
+                    </a> 
                     @endrole
-                    <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-                    <a href='{{ url("/tournois/".$data['tournois']->id."/joueurs") }}'>
-                        Voir les joueurs
-                    </a>
                 </div>
                 @include('tournois/retour')
             </div>
