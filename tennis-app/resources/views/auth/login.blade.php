3388fcbcb5a22383e36c89b71b7d7f89da98b7a9
Gaelle Bouchenot <gaelle.bouchenot@etu.univ-lyon1.fr>
2021-06-16 08:15:47 +0200

    bouton récup mot de passe

--- tennis-app/resources/views/auth/login.blade.php
+++ tennis-app/resources/views/auth/login.blade.php
@@ -52,12 +52,6 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Vous avez oublié votre mot de passe ?') }}
-                    </a>
-                @endif
-
                 <x-button class="ml-3">
                     {{ __('Se connecter') }}
                 </x-button>
